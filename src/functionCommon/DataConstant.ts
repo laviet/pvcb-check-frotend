ae6d200cb72bd0b7fa6c8dc453cde1b97a5a8c16
La Van Viet ( K.NHS - HO) <vietlv@pvcb.vn>
2022-11-04 15:33:00 +0700

    change name app

--- src/functionCommon/DataConstant.ts
+++ src/functionCommon/DataConstant.ts
@@ -1,6 +1,6 @@
 export const pageSizeList = [10, 25, 50];
 export const hightTable = 600;
-// export const urlFEFees="http://10.1.136.185:3001"
-// export const urlBE="http://10.1.136.185:1234/"
-export const urlBE = "https://eway-api-app-nhs-digitech.apps.ocp4-dev03.pvcomtestocp.com/"
-export const urlFEFees = "https://d39v6xu1vwsopp.cloudfront.net"
+export const urlFEFees="http://10.1.136.185:3001"
+export const urlBE="http://10.1.136.185:1234/"
+// export const urlBE = "https://eway-api-app-nhs-digitech.apps.ocp4-dev03.pvcomtestocp.com/"
+// export const urlFEFees = "https://d39v6xu1vwsopp.cloudfront.net"
